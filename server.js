937b2fa49be8db4f28156c86690c4570cc8f3a63
DragonKnight991119 <127013272+DragonKnight991119@users.noreply.github.com>
2023-04-24 20:22:40 +0200

    create repo

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
-const http = require('http')
+// const http = require('http')
 
-const PORT = process.env.PORT || 3000
+// const PORT = process.env.PORT || 3000
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === '/') return respondHello(req, res)
+
+//   res.end()
+// })
+
+// function respondHello (req, res) {
+//   res.end(JSON.stringify({ msg: 'hello' }))
+// }
+
+// server.listen(PORT)
+// console.log(`Server listening on port ${PORT}`)
+
+// if (require.main !== module) module.exports = server
+const http = require('http');
+
+const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') return respondHello(req, res)
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'hello' }));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not found');
+  }
+});
 
-  res.end()
-})
-
-function respondHello (req, res) {
-  res.end(JSON.stringify({ msg: 'hello' }))
-}
-
-server.listen(PORT)
-console.log(`Server listening on port ${PORT}`)
-
-if (require.main !== module) module.exports = server
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
